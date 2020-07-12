f9135e055a61e826b3fa8daa2ccc205802a3d481
Marcus Carvalho <marcuswac@gmail.com>
2020-07-12 15:25:08 -0300

    renaming

--- bin/update_posts.sh
+++ bin/update_posts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Rscript utils/update_posts.R
-cp -r _posts/national/ne_cities/* posts/national/ne_cities/
-cp -r _posts/national/brazil/* posts/national/brazil/
-cp -r _posts/national/deaths_ne_cities/* posts/national/deaths_ne_cities/
-cp -r _posts/national/deaths_brazil/* posts/national/deaths_brazil/
+cp -r _posts/national/ne-cities/ne-cities.html posts/ne-cities/index.html
+cp -r _posts/national/brazil/brazil.html posts/states/index.html
+cp -r _posts/national/deaths-ne-cities/deaths-ne-cities.html posts/deaths-ne-cities/index.html
+cp -r _posts/national/deaths-brazil/deaths_brazil.html posts/deaths_brazil/index.html
