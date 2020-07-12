3b4a9235e4c47e2002153370654ac01ad873ad75
Marcus Carvalho <marcuswac@gmail.com>
2020-07-12 16:13:45 -0300

    Update install_dependencies_apt.sh

--- install_dependencies_apt.sh
+++ install_dependencies_apt.sh
@@ -1,1 +1,1 @@
-apt-get install r-base-core libcurl4-openssl-dev libv8-dev libudunits2-dev libgdal-dev libcairo2-dev
+apt-get install r-base-core libcurl4-openssl-dev libv8-dev libudunits2-dev libgdal-dev libcairo2-dev libfontconfig1-dev
