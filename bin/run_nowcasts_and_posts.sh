4040a7569a4b439f81283b69a1ccc55a79aaf6c7
Marcus Carvalho <marcuswac@gmail.com>
2020-07-20 22:40:08 -0300

    adding Alagoas and Pernambuco

--- bin/run_nowcasts_and_posts.sh
+++ bin/run_nowcasts_and_posts.sh
@@ -13,7 +13,7 @@ cd $ROOT_DIR/_nowcasts/covid-regional
 echo "Starting nowcast for cities at `date`"
 ./update_nowcasts_cities.sh
 
-###################### Nowcast for NE states ######################
+###################### Nowcast for states ######################
 echo "Starting nowcast for states at `date`"
 ./update_nowcasts.sh
 
@@ -26,17 +26,6 @@ bin/update_github.sh
 cd $ROOT_DIR/_nowcasts/covid-regional
 echo "Starting nowcast for BA cities at `date`"
 ./update_nowcasts_state_cities.sh BA
-
-cd $ROOT_DIR
-echo "Updating posts and pushing to github at `date`"
-bin/update_posts.sh
-bin/update_github.sh
-
-################# Deaths nowcast for NE cities and states ###################
-cd $ROOT_DIR/_nowcasts/covid-regional
-echo "Starting deaths nowcast for states at `date`"
-./update_deaths_nowcasts_cities.sh
-./update_deaths_nowcasts.sh
 
 cd $ROOT_DIR
 echo "Updating posts and pushing to github at `date`"
@@ -57,6 +46,37 @@ bin/update_github.sh
 cd $ROOT_DIR/_nowcasts/covid-regional
 echo "Starting nowcast for CE cities at `date`"
 ./update_nowcasts_state_cities.sh CE
+
+cd $ROOT_DIR
+echo "Updating posts and pushing to github at `date`"
+bin/update_posts.sh
+bin/update_github.sh
+
+###################### Nowcast for PE cities ######################
+cd $ROOT_DIR/_nowcasts/covid-regional
+echo "Starting nowcast for PE cities at `date`"
+./update_nowcasts_state_cities.sh PE
+
+cd $ROOT_DIR
+echo "Updating posts and pushing to github at `date`"
+bin/update_posts.sh
+bin/update_github.sh
+
+###################### Nowcast for AL cities ######################
+cd $ROOT_DIR/_nowcasts/covid-regional
+echo "Starting nowcast for AL cities at `date`"
+./update_nowcasts_state_cities.sh AL
+
+cd $ROOT_DIR
+echo "Updating posts and pushing to github at `date`"
+bin/update_posts.sh
+bin/update_github.sh
+
+################# Deaths nowcast for NE cities and states ###################
+cd $ROOT_DIR/_nowcasts/covid-regional
+echo "Starting deaths nowcast for states at `date`"
+./update_deaths_nowcasts_cities.sh
+./update_deaths_nowcasts.sh
 
 cd $ROOT_DIR
 echo "Updating posts and pushing to github at `date`"
