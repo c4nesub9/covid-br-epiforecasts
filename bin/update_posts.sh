8f09a7aeb3811072fea97ee62517cb6543f22d5d
Marcus Carvalho <marcuswac@gmail.com>
2020-07-21 20:52:22 -0300

    new posts

--- bin/update_posts.sh
+++ bin/update_posts.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+Rscript --version
 Rscript utils/update_posts.R
 cp -r _posts/national/ne-cities/ne-cities.html posts/ne-cities/index.html
 cp -r _posts/national/brazil/brazil.html posts/states/index.html
+cp -r _posts/national/cities-al/cities-al.html posts/cities-al/index.html
 cp -r _posts/national/cities-ba/cities-ba.html posts/cities-ba/index.html
 cp -r _posts/national/cities-ce/cities-ce.html posts/cities-ce/index.html
+cp -r _posts/national/cities-ma/cities-ma.html posts/cities-ma/index.html
 cp -r _posts/national/cities-pb/cities-pb.html posts/cities-pb/index.html
+cp -r _posts/national/cities-pe/cities-pe.html posts/cities-pe/index.html
+cp -r _posts/national/cities-pi/cities-pi.html posts/cities-pi/index.html
+cp -r _posts/national/cities-rn/cities-pi.html posts/cities-rn/index.html
 cp -r _posts/national/deaths-brazil/deaths-brazil.html posts/deaths-states/index.html
 cp -r _posts/national/deaths-ne-cities/deaths-ne-cities.html posts/deaths-ne-cities/index.html
