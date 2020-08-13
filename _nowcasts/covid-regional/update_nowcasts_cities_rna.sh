225f978b8eb35d04eb58a642eb2614bdabaa8309
Marcus Carvalho <marcuswac@gmail.com>
2020-08-13 04:03:20 -0300

    update

--- _nowcasts/covid-regional/update_nowcasts_cities_rna.sh
+++ _nowcasts/covid-regional/update_nowcasts_cities_rna.sh
@@ -8,7 +8,7 @@ RESULTS_DIR="brazil/cidades-rna"
 ## Load shared delay from github
 Rscript load_delay.R
 
-Rscript brazil/update_nowcasts_cities.R $RESULTS_DIR brazil/data/cidades_rna.csv 1 1 1
+Rscript brazil/update_nowcasts_cities.R $RESULTS_DIR brazil/data/cidades_rna.csv 1 1
 
 errors=$(./move_regions_with_errors.sh $RESULTS_DIR)
 
